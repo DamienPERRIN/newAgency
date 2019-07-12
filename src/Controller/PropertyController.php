2886fcd2cca4c68e4d312420426a216ffda00624
DamienPERRIN <daperr87@gmail.com>
2019-07-12 23:14:50 +0200

    Finish the second page

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -7,18 +7,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Error\LoaderError;
-use Twig\Error\RuntimeError;
-use Twig\Error\SyntaxError;
 
-class HomeController extends AbstractController
+class PropertyController extends AbstractController
 {
     /**
-     * @Route(path="/", name="home")
+     * @Route(path="/biens", name="property.index")
      * @return Response
      */
     public function index() :Response
     {
-        return $this->render('pages/home.html.twig');
+        return $this->render("property/index.html.twig", [
+            'current_menu' => 'propreties'
+        ]);
     }
 }

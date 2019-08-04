7fc26301c1bad709462fddf574e87f0477a426b1
DamienPERRIN <daperr87@gmail.com>
2019-08-04 17:00:50 +0200

    Adding Create and Edit on my agency. Missing delete part

--- src/Controller/admin/AdminPropertyController.php
+++ src/Controller/admin/AdminPropertyController.php
@@ -5,24 +5,30 @@ namespace App\Controller\admin;
 
 
 use App\Entity\Property;
+use App\Form\PropertyType;
 use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\FormTypeInterface;
 
 /**
+ * @Route("/admin")
  * @property PropertyRepository repository
+ * @property ObjectManager em
  */
 class AdminPropertyController extends AbstractController
 {
-    public function __construct(PropertyRepository $repository)
+    public function __construct(PropertyRepository $repository, ObjectManager $em)
     {
         $this->repository = $repository;
+        $this->em = $em;
     }
 
     /**
-     * @Route("/admin", name="admin.property.index")
+     * @Route("/", name="admin.property.index")
      * @return Response
      */
     public function index()
@@ -32,13 +38,38 @@ class AdminPropertyController extends AbstractController
     }
 
     /**
-     * @Route("/admin/edit/{id}", name="admin.property.edit")
+     * @Route("/property/create", name="admin.property.create")
+     */
+    public function new(Request $request)
+    {
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()&& $form->isValid()){
+            $this->em->persist($property);
+            $this->em->flush();
+            return $this->redirectToRoute("admin.property.index");
+        }
+        return $this->render("admin/property/new.html.twig", [
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="admin.property.edit")
      * @param Property $property
+     * @param Request $request
      * @return Response
      */
-    public function edit(Property $property)
+    public function edit(Property $property, Request $request)
     {
-        $form = $this->createForm(Property::class, $property);
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()&& $form->isValid()){
+            $this->em->flush();
+            return $this->redirectToRoute("admin.property.index");
+        }
         return $this->render("admin/property/edit.html.twig", [
             'property' => $property,
             'form' => $form->createView()
--- src/Form/PropertyType.php
+++ src/Form/PropertyType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Property;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -19,12 +21,14 @@ class PropertyType extends AbstractType
             ->add('bedrooms')
             ->add('floor')
             ->add('price')
-            ->add('heat')
+            ->add('heat', ChoiceType::class, [
+                'choices' => $this->getChoices()
+            ])
             ->add('city')
             ->add('address')
             ->add('postal_code')
             ->add('sold')
-            ->add('created_at')
+            ->add('createdAt', DateType::class)
         ;
     }
 
@@ -32,6 +36,17 @@ class PropertyType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Property::class,
+            'translation_domain' => 'forms'
         ]);
+    }
+
+    private function getChoices()
+    {
+        $choices = Property::HEAT;
+        $output = [];
+        foreach ($choices as $key => $value){
+            $output[$value] = $key;
+        }
+        return $output;
     }
 }

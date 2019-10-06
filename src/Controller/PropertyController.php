b244269b2abbec7351ed64c51f85d683f5711ffa
DamienPERRIN <daperr87@gmail.com>
2019-10-06 17:41:25 +0200

    WIP to send email. Add Entity and form

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -4,8 +4,10 @@
 namespace App\Controller;
 
 
+use App\Entity\Contact;
 use App\Entity\Property;
 use App\Entity\PropertySearch;
+use App\Form\ContactType;
 use App\Form\PropertySearchType;
 use App\Repository\PropertyRepository;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -83,20 +85,35 @@ class PropertyController extends AbstractController
      * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
      * @param Property $property
      * @param string $slug
+     * @param Request $request
      * @return Response
      */
-    public function show(Property $property, string $slug): Response
+    public function show(Property $property, string $slug, Request $request): Response
     {
-//        $property = $this->repository->find($id);
         if ($property->getSlug() !== $slug){
-            return $this->redirectToRoute('property/show.html.twig', [
+            return $this->redirectToRoute('property.show', [
                 'id' => $property->getId(),
                 'slug' => $property->getSlug()
             ], 301);
         }
+
+        $contact = new Contact();
+        $contact->setProperty($property);
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $this->addFlash("success", "Votre email a bien été envoyé");
+            return $this->redirectToRoute('property.show', [
+                'id' => $property->getId(),
+                'slug' => $property->getSlug()
+            ]);
+        }
+
         return $this->render('property/show.html.twig', [
             'property' => $property,
-            'current_menu' => 'properties'
+            'current_menu' => 'properties',
+            'form'         => $form->createView()
         ]);
 
     }
---
+++ src/Entity/Contact.php
@@ -0,0 +1,160 @@
+<?php
+
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Contact
+{
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Length(min="2", max="100")
+     */
+    private $firsname;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Length(min="2", max="100")
+     */
+    private $lastname;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Regex(
+     *     pattern="/[0-9]{10}/"
+     * )
+     */
+    private $phone;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Length(min="10")
+     */
+    private $message;
+
+    /**
+     * @var Property|null
+     */
+    private $property;
+
+    /**
+     * @return string|null
+     */
+    public function getFirsname(): ?string
+    {
+        return $this->firsname;
+    }
+
+    /**
+     * @param string|null $firsname
+     * @return Contact
+     */
+    public function setFirsname(?string $firsname): Contact
+    {
+        $this->firsname = $firsname;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string|null $lastname
+     * @return Contact
+     */
+    public function setLastname(?string $lastname): Contact
+    {
+        $this->lastname = $lastname;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string|null $phone
+     * @return Contact
+     */
+    public function setPhone(?string $phone): Contact
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string|null $email
+     * @return Contact
+     */
+    public function setEmail(?string $email): Contact
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string|null $message
+     * @return Contact
+     */
+    public function setMessage(?string $message): Contact
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    /**
+     * @return Property|null
+     */
+    public function getProperty(): ?Property
+    {
+        return $this->property;
+    }
+
+    /**
+     * @param Property|null $property
+     * @return Contact
+     */
+    public function setProperty(?Property $property): Contact
+    {
+        $this->property = $property;
+        return $this;
+    }
+
+}
